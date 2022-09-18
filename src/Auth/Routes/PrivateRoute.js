42f9af7ba7abde1cf2033c8cbe0163aaebdbe9fa
kynguyenkhac <kynguyenkhac28@gmail.com>
2022-09-18 22:39:40 +0700

    fix bug

--- src/Auth/Routes/PrivateRoute.js
+++ src/Auth/Routes/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Navigate, Outlet } from 'react-router';
 
-export default function PrivateRoute ({userData ,isAllowed, children, redirectPath = '/accessdenied'}){
+export default function PrivateRoute ({userData ,isAllowed, children, redirectPath = '/login'}){
 
   // console.log("-----------USERDATA" ,userData);
   //when reload? userData==null ---fix
